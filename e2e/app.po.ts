c8cfe623b66b4a68f29feef5316ad13540dc7d5b
Frederic Nobre <ext.frederic.nobre@letrot.fr>
2017-01-09 18:13:23 +0100

    back to the roots

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class POCAngularReactPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('poc-angular-react-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {POCAngularReactAppComponent} from './poc-angular-react.component';
