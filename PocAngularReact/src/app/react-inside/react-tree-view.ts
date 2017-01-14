4019e5d8c67c5c6892bcf190e47ce612d974e7cb
Fred N <frederic.nobre@gmail.com>
2017-01-14 18:40:27 +0100

    ok là ça passe

--- PocAngularReact/src/app/react-inside/react-tree-view.ts
+++ PocAngularReact/src/app/react-inside/react-tree-view.ts
@@ -1,7 +1,14 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+
 
 export class ReactTreeView extends React.Component<undefined, undefined> {
     static render(title) {
-        return "<h1>Hello from " + title + "!</h1>";
+        ReactDOM.render(React.createElement(ReactTreeView), document.getElementById('react-tree-view'));
+        //return "<h1>Hello from " + title + "!</h1>";
+    }
+
+    render() {
+        return React.createElement('div', { color: 'red' }, 'react-inside works!');
     }
 }
